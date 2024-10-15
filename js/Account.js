4ce25c2a6b4d82dbe7c1030c352ba6fb9475551c
samueladebayo96 <124725040+samueladebayo96@users.noreply.github.com>
2024-10-15 19:58:03 +0200

    Removed pdf and old script

--- js/Account.js
+++ js/Account.js
@@ -1,41 +1,41 @@
 export default class Account {
-    #solde;
-    #interet;
-    #historiqueOperations = [];
-    constructor(solde) {
-        this.#solde = solde;
-        this.#interet = 0.03;
-    }
-    getSolde() {
-        return this.#solde;
-    }
-    setSolde(newSolde) {
-        this.#solde = newSolde;
-    }
+  #solde;
+  #interet;
+  #historiqueOperations = [];
+  constructor(solde) {
+    this.#solde = solde;
+    this.#interet = 0.03;
+  }
+  getSolde() {
+    return this.#solde;
+  }
+  setSolde(newSolde) {
+    this.#solde = newSolde;
+  }
 
-    deposit(montant) {
-        this.#solde += montant;
-        this.#historiqueOperations.push("Dépôt de " + montant + " euros.");
-    }
+  deposit(montant) {
+    this.#solde += montant;
+    this.#historiqueOperations.push("Dépôt de " + montant + " euros.");
+  }
 
-    withdraw(montant) {
-        if(this.#solde >= montant) {
-            this.#solde -= montant;
-            this.#historiqueOperations.push("Retrait de " + montant + " euros.");
-        }
+  withdraw(montant) {
+    if (this.#solde >= montant) {
+      this.#solde -= montant;
+      this.#historiqueOperations.push("Retrait de " + montant + " euros.");
     }
+  }
 
-    addInteret() {
-        this.#solde += this.#solde *this.#interet;
-    }
+  addInteret() {
+    this.#solde += this.#solde * this.#interet;
+  }
 
-    getInteret() {
-        return this.#interet;
-    }
-    afficherHistorique() {
-        console.log(this.#historiqueOperations);
-    }
-    afficherSolde() {
-        console.log(`Votre solde est de : ${this.#solde} euros.`);
-    }
+  getInteret() {
+    return this.#interet;
+  }
+  afficherHistorique() {
+    console.log(this.#historiqueOperations);
+  }
+  afficherSolde() {
+    console.log(`Votre solde est de : ${this.#solde} euros.`);
+  }
 }
